be1a373ba6a50f89f87601aab8bd7a382bf2cbf7
Benas <work.benasvaran@gmail.com>
2025-09-06 15:32:12 +0300

    Fix PostCSS and tailwind config for vercel deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
