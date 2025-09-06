2366adf24566bbac47d97b465d64444d4750a00c
Benas <work.benasvaran@gmail.com>
2025-09-06 14:51:11 +0300

    middleware and config tweaking

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,5 +34,5 @@ export function middleware(req: NextRequest) {
 
 // Exclude static assets; include API + pages
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
