61f2f895a4398af6e9fe476ff22fa87014fd46da
Benas <work.benasvaran@gmail.com>
2025-09-14 16:31:32 +0300

    Added MCP Tool functionality

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,7 +4,6 @@ import {
   streamText,
   UIMessage,
   validateUIMessages,
-  tool,
   createIdGenerator,
 } from 'ai';
 //import { z } from 'zod';
@@ -12,39 +11,22 @@ import { clearAllMessages, loadChat, saveChat } from '@/app/util/chat-store';
 import { openai } from '@ai-sdk/openai';
 import { after } from 'next/server';
 import { createResumableStreamContext } from 'resumable-stream'
-/*
-// Define your tools
-const tools = {
-  weather: tool({
-    description: 'Get weather information',
-    parameters: z.object({
-      location: z.string(),
-      units: z.enum(['celsius', 'fahrenheit']),
-    }),
-    execute: async ({ location, units }) => {
-      // tool implementation 
-    },
-  }),
-  // other tools
-};
-*/
+import { getMCPClient } from '@/lib/tools';
+
 
 export async function POST(req: Request) {
   //await clearAllMessages();
-  const { message, id }: { message: UIMessage | undefined; id: string; } = await req.json();
+  const { message, id }: { message: UIMessage | undefined; id: string; } = await req.json();  
 
-  
   const { messages: previousMessages } = await loadChat(id);
-
+  
   const messages = [...previousMessages, message!];
 
-  // Validate loaded messages against
-  // tools, data parts schema, and metadata schema
+  const mcpClient = await getMCPClient();
+  const mcpTools = await mcpClient.tools();
+
   const validatedMessages = await validateUIMessages({
     messages,
-    //tools, // Ensures tool calls in messages match current schemas
-    //dataPartsSchema,
-    //metadataSchema,
   });
 
   saveChat({ chatId: id, messages, streamId: null });
@@ -52,16 +34,7 @@ export async function POST(req: Request) {
   const result = streamText({
     model: openai('gpt-4o-mini'),
     messages: convertToModelMessages(validatedMessages),
-    //tools,
-
-    // forward the abort signal:
-    abortSignal: req.signal,
-    onAbort: ({ steps }) => {
-      // Handle cleanup when stream is aborted
-      saveChat({ chatId: id, messages, streamId: null });
-      // Persist partial results to database
-    },    
-
+    tools: mcpTools,
   });
 
   return result.toUIMessageStreamResponse({
@@ -73,7 +46,7 @@ export async function POST(req: Request) {
     onFinish: ({ messages }) => {
      saveChat({ chatId: id, messages, streamId: null });
     },
-        async consumeSseStream({ stream }) {
+    async consumeSseStream({ stream }) {
       const streamId = crypto.randomUUID();
 
       // Create a resumable stream from the SSE stream
---
+++ src/lib/tools.ts
@@ -0,0 +1,10 @@
+import { experimental_createMCPClient as createMCPClient } from 'ai'; 
+
+export async function getMCPClient() {
+    return await createMCPClient({
+        transport: {
+          type: 'sse',
+          url: 'https://mcp.api.coingecko.com/sse',
+        },
+      });
+}
